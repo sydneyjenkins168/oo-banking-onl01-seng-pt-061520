bb43a8502d5d06c1ba687803376b8b3b67c9b03b
Sydney Jenkins <sydjenkins168@gmail.com>
2020-08-06 23:36:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
+  attr_accessor :bank_account, :name, :balance, :status
+  
+  def initialize(bank_account, name=nil, balance=1000, status="open")
+    @bank_account = bank_account
+    @name = name    
+    @balance = balance
+    @status = status
+  end 
+  
 
-end
+  
+  
+end 

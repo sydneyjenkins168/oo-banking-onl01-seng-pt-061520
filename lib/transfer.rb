63d9d1c0e2aa1e013f174e42ce63d4a3d91b3b70
Sydney Jenkins <sydjenkins168@gmail.com>
2020-08-07 00:08:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,11 @@
 class Transfer
-  attr_accessor :transfer, :sender, :receiver, :status, :transfer_amount
+  attr_accessor :sender, :receiver, :status, :amount
   
   def initialize
-    @transfer = transfer 
     @sender = sender 
     @receiver = receiver 
     @status = "pending"
-    @transfer_amount= transfer_amount 
+    @amount= amount 
   end 
   
 end 
